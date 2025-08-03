3b24d07f6256011140cc50372b21c6ffcffa00e6
Abigail Watson <awatson1978@gmail.com>
2025-08-03 03:04:57 -0500

    debugging of CarePlan, Conditions, and Medications

---
+++ server/publications/conditions.js
@@ -0,0 +1,41 @@
+// server/publications/conditions.js
+
+import { Meteor } from 'meteor/meteor';
+import { Conditions } from '/imports/lib/schemas/SimpleSchemas/Conditions';
+
+// Publish all conditions
+Meteor.publish('conditions.all', function() {
+  if (!this.userId) {
+    return this.ready();
+  }
+  
+  console.log('Publishing conditions.all for user:', this.userId);
+  
+  // For now, publish all conditions for logged-in users
+  // In production, you'd want to filter by patient access rights
+  return Conditions.find({});
+});
+
+// Publish a single condition by ID
+Meteor.publish('conditions.byId', function(conditionId) {
+  if (!this.userId || !conditionId) {
+    return this.ready();
+  }
+  
+  console.log('Publishing conditions.byId for ID:', conditionId);
+  
+  return Conditions.find({ _id: conditionId });
+});
+
+// Publish conditions for a specific patient
+Meteor.publish('conditions.byPatient', function(patientId) {
+  if (!this.userId || !patientId) {
+    return this.ready();
+  }
+  
+  console.log('Publishing conditions for patient:', patientId);
+  
+  return Conditions.find({ 
+    'subject.reference': `Patient/${patientId}` 
+  });
+});
